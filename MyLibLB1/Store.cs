e9d620d310a236638f28b5b7e3ae3e48ecc0b0ae
Антон Кузнецов <ant_aa@live.ru>
2022-01-10 17:48:13 +0300

    Добавьте файлы проекта.

---
+++ MyLibLB1/Store.cs
@@ -0,0 +1,87 @@
+﻿namespace MyLibLB1
+{
+    public struct Item
+    {
+        public string name;
+        public bool laid_out;
+        public Item(string name, bool laid_out)
+        {
+            this.name = name;
+            this.laid_out = laid_out;
+        }
+    }
+
+    public class Store
+    {
+        List<Item> list_items;
+        public Store()
+        {
+            list_items = new List<Item>();
+        }
+
+        public void AddItem(string name)
+        {
+            list_items.Add(new Item(name, false));
+        }
+
+        public void DeleteItem(String name)
+        {
+            list_items.Remove(list_items.Find(n => n.name == name));
+        }
+
+        public bool Exists(String name)
+        {
+            return list_items.Exists(n => n.name == name);
+        }
+
+        public int ClearStore()
+        {
+            for (int i = 0; i < list_items.Count; i++)
+            {
+                list_items[i] = new Item(list_items[i].name, false);
+            }
+            return list_items.Count;
+        }
+
+        public void PutItemOnCounter(String name)
+        {
+            Item item = list_items.Find(n => n.name == name);
+            list_items[list_items.IndexOf(item)] = new Item(item.name, true);
+        }
+
+        public int CalculateCountOnCounter()
+        {
+            int result = 0;
+            foreach (Item i in list_items)
+            {
+                if (i.laid_out) result++;
+            }
+            return result;
+        }
+
+        public int CalculateCountNotOnCounter()
+        {
+            int result = list_items.Count - CalculateCountOnCounter();
+            return result;
+        }
+
+        public string GetListAsText()
+        {
+            string text = "";
+            foreach (Item i in list_items)
+            {
+                string status = "не выложено";
+                if (i.laid_out) status = "выложено";
+                text += i.name + " " + status;
+                if (list_items.Count != list_items.IndexOf(i)) text += ", ";
+            }
+            return text;
+        }
+
+        public List<Item> Items
+        {
+            get { return list_items; }
+            set { list_items = value; }
+        }
+    }
+}
---
+++ lb5_02/Controllers/StoreController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyLibLB1;
+
+namespace lb5_02.Controllers
+{
+    public class StoreController : Controller
+    {
+        public static Store store = new Store();
+
+        // GET: StoreController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            ViewBag.action = "add";
+            if (name != null)
+            {
+                if (!store.Exists(name))
+                {
+                    store.AddItem(name);
+                    ViewBag.message_item_name = store.Items[store.Items.Count - 1].name;
+                    ViewBag.message = "Продукт добавлен";
+                    ViewBag.count = store.Items.Count;
+                }
+                else
+                {
+                    ViewBag.message = "Продукт с введенным наименованим уже есть в списке";
+                }
+            }
+            else
+            {
+                ViewBag.message = "Введен пустой текст";
+            }
+            TableList();
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult TableList()
+        {
+            ViewBag.products = store.GetListAsText();
+            ViewBag.count = store.Items.Count;
+            ViewBag.count_on_counter = store.CalculateCountOnCounter();
+            ViewBag.count_not_on_counter = store.CalculateCountNotOnCounter();
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string name)
+        {
+            ViewBag.action = "delete";
+            if (name != null)
+            {
+                if (store.Exists(name))
+                {
+                    store.DeleteItem(name);
+                    ViewBag.message_item_name = name;
+                    ViewBag.message = "Продукт удален";
+                    ViewBag.count = store.Items.Count;
+                }
+                else
+                {
+                    ViewBag.message = "Продукта с введенным наименование нет в списке";
+                }
+            }
+            else
+            {
+                ViewBag.message = "Введен пустой текст";
+            }
+            TableList();
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult PutOn(string name)
+        {
+            ViewBag.action = "PutOn";
+            if (name != null)
+            {
+                if (store.Exists(name))
+                {
+                    store.PutItemOnCounter(name);
+                    ViewBag.message_item_name = name;
+                    ViewBag.message = "Продукт выложен на прилавок";
+                }
+                else
+                {
+                    ViewBag.message = "Продукта с введенным наименование нет в списке";
+                }
+            }
+            else
+            {
+                ViewBag.message = "Введен пустой текст";
+            }
+            TableList();
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult PutOff()
+        {
+            ViewBag.action = "PutOff";
+            if (store.Items.Count != 0)
+            {
+                store.ClearStore();
+                ViewBag.message = "Продукты сняты с прилавка";
+            }
+            else
+            {
+                ViewBag.message = "В магазине нет продуктов";
+            }
+
+            TableList();
+            return View("Index");
+        }
+    }
+}
